ebdb9a5bb3a8a4346b4c70c56ef749e1d4a10a25
Josh Owens <joshua.owens@gmail.com>
2007-12-13 17:35:53 +0000

    Added the html responds block and told layout nil for the xml.  Now we need an html layout.

--- lib/sitemaps_controller.rb
+++ lib/sitemaps_controller.rb
@@ -1,12 +1,12 @@
 class SitemapsController < ActionController::Base
-  layout nil
   
   def show
     s = Queso::SEO::Sitemap.new
     @widgets = s.widgets
     @sitemap_config = s.sitemap_config
     respond_to do |format|
-      format.xml {render :template => "show.xml.builder"}
+      format.html
+      format.xml {render :template => "show.xml.builder", :layout => false}
     end
   end
   
