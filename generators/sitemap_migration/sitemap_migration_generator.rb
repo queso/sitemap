5d5e2b8ba8052808d79e3404aa13d2955b0621a0
Josh Owens <joshua.owens@gmail.com>
2007-12-21 15:38:32 +0000

    Adding the start of the generator.

---
+++ generators/sitemap_migration/sitemap_migration_generator.rb
@@ -0,0 +1,14 @@
+class SitemapMigrationGenerator < Rails::Generator::NamedBase
+  attr_reader :sitemap_table_name
+  def initialize(runtime_args, runtime_options = {})
+    @sitemap_table_name = (runtime_args.length < 2 ? 'sitemap' : runtime_args[1]).tableize
+    runtime_args << 'add_sitemap_table' if runtime_args.empty?
+    super
+  end
+
+  def manifest
+    record do |m|
+      m.migration_template 'migration.rb', 'db/migrate'
+    end
+  end
+end
