90387590c98c825adc4262431a7c946355e56f4a
Josh Owens <joshua.owens@gmail.com>
2007-12-31 17:39:49 +0000

    Adding the new/edit form for widgets.  The index page still needs some tweaks.

--- lib/sitemap_widget.rb
+++ lib/sitemap_widget.rb
@@ -1,3 +1,11 @@
 class SitemapWidget < ActiveRecord::Base
   
+  def self.priorities
+    [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+  end
+  
+  def self.frequencies
+    {"Always" => "always", "Daily" => "daily", "Weekly" => "weekly", "Monthly" => "monthly", "Yearly" => "yearly"}
+  end
+  
 end
