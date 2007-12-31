e956fd84fa1377c3637242cd9b7af0ff5d7be0f2
Josh Owens <joshua.owens@gmail.com>
2007-12-31 17:59:07 +0000

    Adding static links controller + views

--- init.rb
+++ init.rb
@@ -3,3 +3,4 @@ require 'sitemap_fu'
 SitemapsController.view_paths = [File.join(directory, 'views')]
 SitemapSettingsController.view_paths = [File.join(directory, 'views')]
 SitemapWidgetsController.view_paths = [File.join(directory, 'views')]
+SitemapStaticLinksController.view_paths = [File.join(directory, 'views')]
---
+++ lib/sitemap_static_links_controller.rb
@@ -0,0 +1,29 @@
+class SitemapStaticLinksController < ActionController::Base
+  
+  def index
+    @static_links = SitemapStaticLink.find(:all)
+  end
+  
+  def edit
+    @static_link = SitemapStaticLink.find(params[:id])
+  end
+  
+  def update
+    @static_link = SitemapStaticLink.find(params[:id])
+    if @static_link.update_attributes(params[:sitemap_static_link])
+      redirect_to sitemap_static_links_url
+    end
+  end
+  
+  def create
+    @static_link = SitemapStaticLink.new(params[:sitemap_static_link])
+    if @static_link.save
+      redirect_to sitemap_static_links_url
+    end
+  end
+  
+  def new
+    @static_link = SitemapStaticLink.new()
+  end
+  
+end
