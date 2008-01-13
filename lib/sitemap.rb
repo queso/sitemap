b3cfc3d55ce59c380b6ed8a1588c3d79c496fe84
Josh Owens <joshua.owens@gmail.com>
2008-01-13 03:57:04 +0000

    Adding my routes... Hope this works, it will be awesomness!

--- lib/sitemap.rb
+++ lib/sitemap.rb
@@ -1,42 +1,13 @@
 Mime::Type.register_alias "text/xml", :xsl
 
-module Queso
-  module SEO
-    
-    class ConfigFileNotFoundError < StandardError; end
-    
-    class Sitemap
-      attr_accessor :sitemap_config
-    
-      def initialize(file = '/config/sitemap.yml') #:nodoc:
-        begin
-          path = RAILS_ROOT + file
-          @sitemap_config = YAML.load_file(path).symbolize_keys
-        rescue
-          raise ConfigFileNotFoundError.new('File %s not found' % path )
-        end
-      end
-    
-      def widgets
-        widgets = {}
-        sitemap_config[:widgets].each_key do |k|
-          widgets[k] = find_widgets(sitemap_config[:widgets][k]["model"].constantize)
-        end
-        widgets
-      end
-    
-      def find_widgets(widget_model, options = {})
-        widget_model.find(:all, options)
-      end
-      
-      def site
-        site = sitemap_config[:site].symbolize_keys
-      end
-      
-      def static_links
-        static_links = sitemap_config[:static_links].symbolize_keys
-      end
-      
-    end
-  end
+
+class << ActionController::Routing::Routes;self;end.class_eval do
+  define_method :clear!, lambda {}
+end
+
+ActionController::Routing::Routes.draw do |map|
+  map.resource :sitemap
+  map.resources :sitemap_settings, :collection => {:widgets => :get}
+  map.resources :sitemap_widgets
+  map.resources :sitemap_static_links
 end
